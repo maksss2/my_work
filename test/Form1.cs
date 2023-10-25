d9c50447281264b9e4256b53251d7aed8eb38e18
0_306_3 <0_306_3@1_306_4>
2023-10-25 14:42:15 +0500

    #1

--- test/Form1.cs
+++ test/Form1.cs
@@ -12,9 +12,40 @@ namespace test
 {
     public partial class Form1 : Form
     {
+        Random random = new Random();
+            List<string> icons = new List<string>()
+            {
+"!", "!","N","N",",",",","k","k","b","b","v","v","w","z","z"
+            //рандомайз иконок
+            };
+        
         public Form1()
         {
             InitializeComponent();
+        }
+       private void AssingIconsToSquares()
+        {
+            foreach (Control control in tableLayoutPanel1.Controls)
+            {
+                Label iconLabel1 = control as Label;
+                if (iconLabel1 != null)
+                {
+                    int randomNumber = random.Next(icons.Count);
+                    iconLabel1.Text = icons[randomNumber];
+                    icons.RemoveAt(randomNumber);
+                }
+            }
+
+
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
