322b6da242be4057b317c13ca9262da5da5cd7ee
мистер варфейс 228 <226niger228@gmail.com>
2023-11-17 15:00:10 +0500

    #1

--- test/Form1.Designer.cs
+++ test/Form1.Designer.cs
@@ -29,12 +29,83 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pct = new System.Windows.Forms.PictureBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.tmr = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pct)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pct
+            // 
+            this.pct.Image = ((System.Drawing.Image)(resources.GetObject("pct.Image")));
+            this.pct.InitialImage = null;
+            this.pct.Location = new System.Drawing.Point(29, 12);
+            this.pct.Name = "pct";
+            this.pct.Size = new System.Drawing.Size(218, 383);
+            this.pct.TabIndex = 0;
+            this.pct.TabStop = false;
+            this.pct.WaitOnLoad = true;
+            this.pct.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(12, 401);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(112, 401);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            // 
+            // tmr
+            // 
+            this.tmr.Tick += new System.EventHandler(this.tmr_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(430, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(359, 383);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(820, 480);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.pct);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pct)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pct;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Timer tmr;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- test/Form1.cs
+++ test/Form1.cs
@@ -12,9 +12,30 @@ namespace test
 {
     public partial class Form1 : Form
     {
+        
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void tmr_Tick(object sender, EventArgs e)
+        {//движение картинки
+            pct.Left = pct.Left + 20;
+            
+                 }
+        
+        private void btnStart_Click(object sender, EventArgs e)
+        {/* по сути тут можно сделать так
+         тчо бы картинка останавливалсь по истеченю времент
+         взять что то из прошлой работы*/
+            tmr.Enabled = true;
+            tmr.Interval = 50;
+            Console.WriteLine(tmr);
+        }
+        
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
